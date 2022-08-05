bcd4a5305ff6fee6747317e25f576501f30c7854
naguib-med <naguib.mohamed1999@gmail.com>
2022-08-05 06:33:46 +0200

    mise en place de l'édition d'article

--- src/form/form.js
+++ src/form/form.js
@@ -3,7 +3,43 @@ import "./form.scss";
 
 const form = document.querySelector("form");
 const errorElement = document.querySelector("#errors");
+const btnCancel = document.querySelector(".btn-secondary");
+let articleId;
 let errors = [];
+
+const fillForm = (article) => {
+  const author = document.querySelector("input[name='author']");
+  const img = document.querySelector("input[name='img']");
+  const category = document.querySelector("input[name='category']");
+  const title = document.querySelector("input[name='title']");
+  const content = document.querySelector("textarea[name='content']");
+
+  author.value = article.author || "";
+  img.value = article.img || "";
+  category.value = article.category || "";
+  title.value = article.title || "";
+  content.value = article.content || "";
+};
+
+const initForm = async () => {
+  const params = new URL(location.href);
+  articleId = params.searchParams.get("id");
+
+  if (articleId) {
+    const response = await fetch(`https://restapi.fr/api/article/${articleId}`);
+
+    if (response.status < 300) {
+      const article = await response.json();
+      fillForm(article);
+    }
+  }
+};
+
+initForm();
+
+btnCancel.addEventListener("click", () => {
+  location.assign("/index.html");
+});
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
@@ -12,23 +48,30 @@ form.addEventListener("submit", async (event) => {
   if (formIsValid(article)) {
     try {
       const json = JSON.stringify(article);
-      const response = await fetch("https://restapi.fr/api/article", {
-        method: "POST",
-        body: json,
-        headers: { "Content-Type": "application/json" },
-      });
-
-      const body = await response.json();
-      console.log(body);
+      let response;
+      if (articleId) {
+        response = await fetch(`https://restapi.fr/api/article/${articleId}`, {
+          method: "PUT",
+          body: json,
+          headers: { "Content-Type": "application/json" },
+        });
+        if (response.status < 299) {
+          location.assign("/index.html");
+        }
+      } else {
+        response = await fetch("https://restapi.fr/api/article", {
+          method: "POST",
+          body: json,
+          headers: { "Content-Type": "application/json" },
+        });
+        if (response.status < 299) {
+          location.assign("/index.html");
+        }
+      }
     } catch (error) {
       console.log("error: ", error);
     }
   }
-
-  //   const array = Array.from(entries).reduce((acc, entry) => {
-  //     acc[entry[0]] = entry[1];
-  //     return acc;
-  //   }, {});
 });
 
 const formIsValid = (article) => {
@@ -41,6 +84,8 @@ const formIsValid = (article) => {
     !article.title
   ) {
     errors.push("Vous devez renseigner tous les champs");
+  } else {
+    errors = [];
   }
   if (article.content.length < 20) {
     errors.push("Le contenu de votre article est trop court !");
--- src/index.js
+++ src/index.js
@@ -9,21 +9,29 @@ const createArticles = (articles) => {
     const articleDOM = document.createElement("div");
     articleDOM.classList.add("article");
     articleDOM.innerHTML = `
-    <img
-      src="${article.img}"
-      alt="profile"
-    />
-    <h2>${article.title}</h2>
-    <p class="article-author">${article.author} - ${article.category}</p>
-    <p class="article-content">
-     ${article.content}
-    </p>
-
-    <div class="article-actions">
-      <button class="btn btn-danger" data-id=${article._id}>Supprimer</button>
-    </div>
-
-    `;
+      <img
+        src="${article.img}"
+        alt="profile"
+      />
+      <h2>${article.title}</h2>
+      <p class="article-author">${article.author} - ${new Date(
+      article.createdAt
+    ).toLocaleDateString("fr-FR", {
+      weekday: "long",
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+    })}</p>
+      <p class="article-content">
+       ${article.content}
+      </p>
+  
+      <div class="article-actions">
+        <button class="btn btn-primary" data-id=${article._id}>Modifier</button>
+        <button class="btn btn-danger" data-id=${article._id}>Supprimer</button>
+      </div>
+  
+      `;
 
     return articleDOM;
   });
@@ -32,11 +40,19 @@ const createArticles = (articles) => {
   articleContainer.append(...articlesDOM);
 
   const deleteButtons = articleContainer.querySelectorAll(".btn-danger");
+  const editButtons = articleContainer.querySelectorAll(".btn-primary");
+
+  editButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const target = event.target;
+      const articleId = target.dataset.id;
+      location.assign(`/form.html?id=${articleId}`);
+    });
+  });
 
   deleteButtons.forEach((button) => {
     button.addEventListener("click", async (event) => {
       try {
-        console.log(event.target.dataset.id);
         const target = event.target;
         const articleId = target.dataset.id;
         const response = await fetch(
@@ -59,7 +75,13 @@ const fetchArticle = async () => {
   try {
     const response = await fetch("https://restapi.fr/api/article");
     const articles = await response.json();
-    createArticles(articles);
+    console.log(articles);
+    // articles.isArray ? createArticles([articles]) : createArticles(articles);
+    if (articles.length > 1) {
+      createArticles(articles);
+    } else {
+      createArticles([articles]);
+    }
   } catch (error) {
     console.log("error", error);
   }
